38975cdbcf5282a90660826658b1e747457f724f
Peter Kurzok <peter.kurzok@maibornwolff.de>
2025-02-17 23:23:18 +0100

    Configure Hostname

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,6 +2,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    app.http.server.configuration.hostname = "0.0.0.0"
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
